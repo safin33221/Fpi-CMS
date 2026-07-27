3ebc42174ed9c83b397d0db7f7a8aff666d25a8d
safin33221 <safin33221@gmail.com>
2026-07-28 03:27:08 +0600

    feat: add PrincipalMessage component to display a welcome message from the principal

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,9 +1,11 @@
 import Hero from "@/components/module/public/home/Hero";
+import { PrincipalMessage } from "@/components/module/public/home/PrincipalMessage";
 
 export default function Home() {
   return (
     <div>
       <Hero />
+      <PrincipalMessage />
     </div>
   );
 };
